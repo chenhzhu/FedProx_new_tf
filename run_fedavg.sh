045065b38236d454c2c28d27155ade0dfe8fd144
chenhzhu <chenhzhu@umich.edu>
2025-03-26 14:09:27 -0700

    Add check gpu device

--- run_fedavg.sh
+++ run_fedavg.sh
@@ -6,3 +6,10 @@ python3 -u main.py --dataset=$1 --optimizer='fedavg'  \
             --model='mclr' \
             --drop_percent=$2 \
 
+# MNIST
+# python3 -u main.py --dataset='mnist' --optimizer='fedavg'  \
+#             --learning_rate=0.01 --num_rounds=200 --clients_per_round=10 \
+#             --eval_every=1 --batch_size=10 \
+#             --num_epochs=20 \
+#             --model='mclr' \
+#             --drop_percent=$2 \
--- test_tf.py
+++ test_tf.py
@@ -1,4 +1,16 @@
 
 import tensorflow as tf
+from tensorflow.python.client import device_lib
+
 
 print(tf.__version__)
+tf.config.list_physical_devices('GPU')
+print(device_lib.list_local_devices())
+gpu_device_name = tf.test.gpu_device_name()
+print(gpu_device_name)
+
+local_device_protos = device_lib.list_local_devices()
+# print(local_device_protos)
+ 
+# Only print GPU devices.
+[print(x) for x in local_device_protos if x.device_type == 'GPU']
